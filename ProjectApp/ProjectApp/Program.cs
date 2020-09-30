7b55be1f531f55de6d69f78becf34a9cfac7f401
Sam Haimes <samhaimes@hotmail.com>
2020-09-30 12:31:04 +0100

    Created the project in Visual Studio. Added a Model, Business Front, Testing and WPF.

---
+++ ProjectApp/ProjectApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
